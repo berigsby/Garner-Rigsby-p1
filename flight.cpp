61ad683663507f3554bc2eb80629dc29bd5c230b
JALYSA GARNER <garner@nike.cs.uga.edu>
2017-01-31 18:46:32 -0500

    added to the flight .cpp and header file

--- flight.cpp
+++ flight.cpp
@@ -5,5 +5,42 @@
 #include <sstream>
 using namespace std;
 
-//flight::flight(int maxPassenger,int flightNo){
-//}
+/*have to come back to the that*/
+flight::flight(int maxPassenger,int flightNo){
+}
+
+/*if i want to reset my list, we can reset the current position to 0, so that 
+  way all odd data will just be overwritten*/
+void flight::ResetList(){
+  currentPos = 0;
+}
+/*just returning the position we are at in the array, which equates to how 
+  many passengers we have */
+int flight::GetNumPassengerInFlight() const{
+  return currentPos+1;
+}
+
+void flight::addPassenger(passenger passenger){
+  passengerList[currentPos] = passenger;
+  currentPos++;
+}
+
+void flight::removePassenger(passenger passenger){
+  for(int i =0; i<currentPos; i++){
+    if(passengerList[i].flightNo == passenger.flightNo){
+      passengerList[i] = passengerList[currentPos];
+      currentPos++;
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
