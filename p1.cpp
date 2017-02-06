c03636ca76a8738962d349d37cff6dc5861d5143
JALYSA GARNER <garner@nike.cs.uga.edu>
2017-02-06 15:05:02 -0500

    did task 1-3 working on 4

--- p1.cpp
+++ p1.cpp
@@ -11,9 +11,24 @@ using namespace std;
 int main(){
   string line;
   int number = 1;
-  Flight f(50,50);
+  /*TASK ONE*/ 
+ /************************************************************************************************/
+ Flight f(50,1);
+ Flight f2(50,2);
+ Flight f3(50,3);
+ Flight f4(50,4);
+ Flight f5(50,5);
 
-  ifstream myfile("passengers.dat");
+ cout <<"Flight f has 50 max passengers and flight No is 1"<<endl;
+ cout <<"Flight f2 has 50 max passengers and flight No is 2"<<endl;
+ cout <<"Flight f3 has 50 max passengers and flight No is 3"<<endl;
+ cout <<"Flight f4 has 50 max passengers and flight No is 4"<<endl;
+ cout <<"Flight f5 has 50 max passengers and flight No is 5"<<endl; 
+ /************************************************************************************************/
+
+ /*TASK TWO*/
+ /************************************************************************************************/
+ ifstream myfile("passengers.dat");
   if(myfile.is_open()){
     while(getline (myfile,line)){
       stringstream iss(line);
@@ -24,16 +39,46 @@ int main(){
       Passenger p(number,lastname,firstname,number);
       f.addPassenger(p);
       number++;
-    }//while
+      lastname += "-Alt";
+      
+      Passenger p1(number,lastname,firstname,number);
+      f2.addPassenger(p1);
+      number++;
+      lastname += "-Alt";
+
+      Passenger p2(number,lastname,firstname,number);
+      f3.addPassenger(p2);
+      number++;
+      lastname += "-Alt";
+
+      Passenger p3(number,lastname,firstname,number);
+      f4.addPassenger(p3);
+      number++;
+      lastname += "-Alt";
+
+      Passenger p4(number,lastname,firstname,number);
+      f5.addPassenger(p4);
+      }//while
     myfile.close();
   }//if
   //f.showAllPassengers();
+  /********************************************************************************/
 
+  /*TASK THREE*/
+  if(f.IsFlightFull()== 1){
+    cout<<"Flight "<<f.flightNo<<" is full"<<endl;
+  }
+
+
+  /*TASK FOUR*/
+
+
+  //Passenger p1(13,"Garner","Jalysa",13);
+  //f.removePassenger(p1);
   AirDB listy;
+  // listy.addFlight(f);
   listy.addFlight(f);
-  listy.addFlight(f);
-
   listy.showAllFlightsAndPassengers();
-  f.showAllPassengers();
+  // f.showAllPassengers();
   return 0;
 }
